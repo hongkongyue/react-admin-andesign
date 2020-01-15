906b355eca038f083816129af22ba2691448b324
洪坤越 <hongkunyue.123@gmail.com>
2020-01-15 14:56:48 +0800

    ui

--- src/page/sider.js
+++ src/page/sider.js
@@ -16,44 +16,44 @@ export default class Sider extends React.Component {
 		console.log('点击了',e)
 	}
 	componentWillMount() {
-        const menuList = this.renderMenu(menuConfig);
+        const menuList = this.recursionMenu(menuConfig);
         this.setState({menuList})
     }
-	//使用递归
-    renderMenu = (data) => {
-        return  data.map((item) => {
-            if (!!item.children&&item.children.length>0) {
-                return (
-                    <SubMenu key={item.id}  title={<span><Icon type={item.type}/><span>{item.title}</span></span>}>
-                         {this.renderMenu(item.children)}
-                    </SubMenu>
-                )
-            } else {
-                return (
-                    <MenuItem  key={item.name}  title={item.title}>
-                           <Link to = {item.name}>{item.title}</Link>
-                    </MenuItem>
-                )
-            }
-        })
-	};
+	// //使用递归
+    // renderMenu = (data) => {
+    //     return  data.map((item) => {
+    //         if (!!item.children&&item.children.length>0) {
+    //             return (
+    //                 <SubMenu key={item.id}  title={<span><Icon type={item.type}/><span>{item.title}</span></span>}>
+    //                      {this.renderMenu(item.children)}
+    //                 </SubMenu>
+    //             )
+    //         } else {
+    //             return (
+    //                 <MenuItem  key={item.name}  title={item.title}>
+    //                        <Link to = {item.name}>{item.title}</Link>
+    //                 </MenuItem>
+    //             )
+    //         }
+    //     })
+	// };
 	//递归循环左侧菜单栏按钮
-	renderMenu=(data)=>{
-        return data.map((item)=>{
-                if(!!item.children&&item.children.length>0){
-					return (
-						<SubMenu key={item.id} title={<span><Icon type="item.type"/><span>{item.title}</span></span>}>
-								{this.renderMenu(item.children)}
-						</SubMenu>
-					)
-				}else{
-					return(<MenuItem key={item.id} title={item.title}> <Link to = {item.name}>{item.title}</Link></MenuItem>)	
-				}
-		 })  
+	recursionMenu=(data)=>{
+		   return data.map((item)=>{
+			       if(!!item.children&&item.children.length>0){
+					   return (
+					      <SubMenu key={item.id} title={<span><Icon type={item.type}/>{item.title}</span>}>
+							      {this.recursionMenu(item.children)}
+						  </SubMenu>
+					   )
+				   }else{
+					    return(<MenuItem key={item.id} title={item.title}> <Link to = {item.name}>{item.title}</Link></MenuItem>) 
+				   }
+		   }) 
 	}
     render () {
 				return (
-						 <Menu theme='dark' onClick={this.handleClick} onChange = {this.getValue} style={{height:'93.2vh',overflowY:'hidden'}} mode="inline">
+						 <Menu theme='dark' onClick={this.handleClick}  onChange ={this.getValue} style={{height:'93.2vh',overflowY:'hidden'}} mode="inline">
                              {this.state.menuList}
                          </Menu>
 				);
